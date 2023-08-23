e4e0388502980e5d5dec4fcbe9ea454c65ab8496
Saurav Adhikari <sauravads123@gmail.com>
2023-08-23 21:04:58 +0545

    :wrench: feat : add support for env file

---
+++ lib/env.go
@@ -0,0 +1,33 @@
+package lib
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Env struct {
+	CognitoAppClientId string `mapstructure:"COGNITO_APP_CLIENT_ID"`
+	CognitoRegion      string `mapstructure:"COGNITO_REGION"`
+}
+
+// Will be populated by
+var env = Env{}
+
+func GetEnv() Env {
+	return env
+}
+
+func NewEnv() *Env {
+	viper.SetConfigFile(".env")
+	viper.SetDefault("TIMEZONE", "UTC")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&env)
+	if err != nil {
+		panic(err)
+	}
+
+	return &env
+}
